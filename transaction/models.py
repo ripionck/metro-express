6621511d36c7fa04cbb1d9d455770ec78d25f126
Ripion Chakma <ripionchakma.421@gmail.com>
2024-01-18 02:09:19 +0600

    added transaction model and registered it

--- transaction/models.py
+++ transaction/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='transactions')
+    amount = models.DecimalField(max_digits=10, decimal_places=2,null=True, blank=True)
+    balance_after_transaction = models.DecimalField(max_digits=10, decimal_places=2,null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.amount}'
