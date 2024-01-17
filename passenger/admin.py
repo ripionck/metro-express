f96f61b74f1d32c21c4fe3f62e2cec314eb14570
Ripion Chakma <ripionchakma.421@gmail.com>
2024-01-18 02:05:46 +0600

    added passenger model and registered it

--- passenger/admin.py
+++ passenger/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+class PassengerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'nid_no', 'mobile_no']
+
+    def first_name(self, obj):
+        return obj.user.first_name
+    
+    def last_name(self, obj):
+        return obj.user.last_name
+    
+admin.site.register(models.Passenger, PassengerAdmin)
--- passenger/models.py
+++ passenger/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from .constants import GENDER_TYPE
+
 
 # Create your models here.
+class Passenger(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='passengers')
+    nid_no = models.CharField(max_length=10, unique=True)
+    mobile_no = models.CharField(max_length = 12, unique=True)
+    date_of_birth = models.DateField()
+    gender = models.CharField(choices=GENDER_TYPE, max_length=20)
+    house_no = models.IntegerField()
+    road_no = models.CharField(max_length=50)
+    zip_code = models.IntegerField()
+    city = models.CharField(max_length=20)
+    balance = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
