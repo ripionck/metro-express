8e8343df90626cefc77dc74f184848c191df1634
Ripion Chakma <ripionchakma.421@gmail.com>
2024-01-18 02:07:35 +0600

    added booking model and registered it

---
+++ booking/constants.py
@@ -0,0 +1,7 @@
+SEAT_CLASS_TYPES = [
+        ('Economy', 'Economy'),
+        ('FirstClass', 'First Class'),
+        ('Business', 'Business'),
+        ('Premium', 'Premium'),
+        ('Luxury', 'Luxury'),
+    ]
