85c77b981d8881703b1a1e71516e62fd1ee81c4a
Ripion Chakma <ripionchakma.421@gmail.com>
2024-01-19 03:02:51 +0600

    edited profile template

--- passenger/forms.py
+++ passenger/forms.py
@@ -78,6 +78,16 @@ class ProfileUpdateForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # Add CSS classes to form fields for styling
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class': (
+                    'appearance-none block w-full bg-gray-200 '
+                    'text-gray-700 border border-gray-200 rounded '
+                    'py-3 px-4 leading-tight focus:outline-none '
+                    'focus:bg-white focus:border-gray-500'
+                )
+            })
 
         if self.instance:
             try:
