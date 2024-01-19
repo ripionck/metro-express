ce92fcb28598dd0cf862c77a96de92645ddb4d01
Ripion Chakma <ripionchakma.421@gmail.com>
2024-01-19 07:04:24 +0600

    added requirements.txt file

--- metro_express/settings.py
+++ metro_express/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-ing257(r3a#1xa=a-1%470@a)k1wocb@mhz%((i8=q$*f11f*y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
