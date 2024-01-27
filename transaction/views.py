bf5dc41816e0e661de729b93b4e9ecabca0edee4
Ripion Chakma <ripionchakma.421@gmail.com>
2024-01-27 14:58:24 +0600

    remove all print function

--- transaction/views.py
+++ transaction/views.py
@@ -15,7 +15,6 @@ class DepositView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         amount = form.cleaned_data.get('amount')
         passenger = self.request.user.passenger_profile
-        print(passenger.balance)
         passenger.balance += amount
         passenger.save(
             update_fields=[
