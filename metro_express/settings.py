d1aa05ecbbf4f6f482fba6205f458aeeec38c446
Ripion Chakma <ripionchakma.421@gmail.com>
2024-02-29 20:40:36 +0600

    security warning turned off

--- metro_express/settings.py
+++ metro_express/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ["https://metro-express.onrender.com", "http://127.0.0.1:8080"]
